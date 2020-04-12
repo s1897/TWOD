463a3d5c06a2379f61aa9a0417c968f213b7f02c
s1897 <tillheim@bluewin.ch>
2020-04-12 13:13:12 +0200

    .

--- TWOD_RCI/RCI.py
+++ TWOD_RCI/RCI.py
@@ -13,4 +13,5 @@ rcin = RCInput()
 #     return int(rcin.read(rc_channl))
 
 def rci(rc_channl, rc_drx=-1, rc_urx=1, rc_di=982, rc_ui=2006):
-    return rci_enc(int(rcin.read(rc_channl)), rc_drx, rc_urx, rc_di, rc_ui)
+    h = int(rcin.read(rc_channl))
+    return rci_enc(h, rc_drx, rc_urx, rc_di, rc_ui)
