464e78c1688e98ca47c41f06c65df2cadc37e277
s1897 <tillheim@bluewin.ch>
2020-04-12 16:46:10 +0200

    .

--- MAI.py
+++ MAI.py
@@ -2,5 +2,6 @@ from TWOD_RCI.RCI import rci
 from TWOD_PWM.PWM import pwm
 
 while True:
-    pwm(0, rci(0))
+    H = pwm(0, rci(0))
+    print(H)
     # print("{0}  {1}   {2}  {3}  {4}  {5}  {6}  {7}  {8}  {9}".format(rci(0), rci(1), rci(2), rci(3), rci(4), rci(5), rci(6), rci(7), rci(8), rci(9)))
