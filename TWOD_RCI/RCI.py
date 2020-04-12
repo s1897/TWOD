e43aff8a0954152a1ee2be6086db5b6272cfe5e9
s1897 <tillheim@bluewin.ch>
2020-04-12 03:12:25 +0200

    .

--- TWOD_RCI/RCI.py
+++ TWOD_RCI/RCI.py
@@ -8,4 +8,4 @@ from RCI_ENC import rci_enc
 
 
 def rci(rc_channl, rc_drx=-1, rc_urx=1, rc_di=982, rc_ui=2006):
-    return rci_enc(flot(RCInput().read(rc_channl)), rc_drx, rc_urx, rc_di, rc_ui)
+    return rci_enc(float(RCInput().read(rc_channl)), rc_drx, rc_urx, rc_di, rc_ui)
