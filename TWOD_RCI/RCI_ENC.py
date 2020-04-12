8700c7a4c432c501c26d46c68e1945e271bc17ec
s1897 <tillheim@bluewin.ch>
2020-04-12 13:32:07 +0200

    .

--- TWOD_RCI/RCI_ENC.py
+++ TWOD_RCI/RCI_ENC.py
@@ -1,11 +1,11 @@
 
 
 def rci_enc(rc_rx, rc_drx=-1, rc_urx=1, rc_di=982, rc_ui=2006):
-    print(rc_rx)
 
     rc_sr = rc_ui - rc_di
 
-    return (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx
+    print(rc_rx, (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx)
+    # return (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx
 
 
 if __name__ == '__main__':
