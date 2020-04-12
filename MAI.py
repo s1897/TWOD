bcff9c899db156a3d9d4abcbfe854d9915a0ce8b
s1897 <tillheim@bluewin.ch>
2020-04-12 13:50:07 +0200

    .

--- MAI.py
+++ MAI.py
@@ -1,4 +1,12 @@
 from TWOD_RCI.RCI import rci
 
 while True:
-    print(rci(0))
+    receiver_imput = rci(0)
+    Convertet_output_lowes = -1
+    Convertet_output_highest = 1
+    receiver_imput_lowest = 982
+    receiver_imput_highest = 2006
+
+    receiver_signal_range = receiver_imput_highest - receiver_imput_lowest
+
+    return (((Convertet_output_highest - Convertet_output_lowes) / receiver_signal_range) * (receiver_signal_range - (receiver_imput_highest - receiver_imput))) + Convertet_output_lowes
--- TWOD_RCI/RCI.py
+++ TWOD_RCI/RCI.py
@@ -1,23 +1,12 @@
 
 from TWOD_NA2.rcinput import *
 from TWOD_NA2.util import *
-# from RCI_ENC import *
+from RCI_ENC import *
 
 check_apm()
 
 rcin = RCInput()
 
 
-# def rci(rc_channl):
-#     return int(rcin.read(rc_channl))
-
 def rci(rc_channl):
-    receiver_imput = int(rcin.read(rc_channl))
-    Convertet_output_lowes = 0
-    Convertet_output_highest = 1
-    receiver_imput_lowest = 982
-    receiver_imput_highest = 2006
-
-    receiver_signal_range = receiver_imput_highest - receiver_imput_lowest
-
-    return (((Convertet_output_highest - Convertet_output_lowes) / receiver_signal_range) * (receiver_signal_range - (receiver_imput_highest - receiver_imput))) + Convertet_output_lowes
+    return int(rcin.read(rc_channl))
