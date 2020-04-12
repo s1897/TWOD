c109b5efb952c4481e061cd8e5d47e367be29e05
s1897 <tillheim@bluewin.ch>
2020-04-12 14:57:08 +0200

    .

--- MAI.py
+++ MAI.py
@@ -1,4 +1,4 @@
 from TWOD_RCI.RCI import rci
 
 while True:
-    print("{0}  {1}   {2}  {3}  {4}".format(rci(0), rci(1), rci(2), rci(3), rci(4)))
+    print("{0}  {1}   {2}  {3}  {4}  {5}  {6}  {7}".format(rci(0), rci(1), rci(2), rci(3), rci(4), rci(5), rci(6), rci(7)))
