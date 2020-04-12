5f8b2a533798cef44bad5557321bfc332e0803e1
s1897 <tillheim@bluewin.ch>
2020-04-12 13:35:58 +0200

    .

--- TWOD_RCI/RCI_ENC.py
+++ TWOD_RCI/RCI_ENC.py
@@ -4,9 +4,10 @@ def rci_enc(rc_rx, rc_drx=-1, rc_urx=1, rc_di=982, rc_ui=2006):
 
     rc_sr = rc_ui - rc_di
 
-    print(rc_rx, (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx)
+    print(rc_rx + 1000, (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx)
     # return (((rc_urx - rc_drx) / rc_sr) * (rc_sr - (rc_ui - rc_rx))) + rc_drx
 
 
 if __name__ == '__main__':
-    print(rci_enc((2006 - 982) / 2 + 982))
+    print(rci_enc(int(1400)))
+    print(rci_enc(int(982)))
