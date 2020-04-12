99e9506918a37e106463335373ad879fb299f60e
s1897 <tillheim@bluewin.ch>
2020-04-12 14:43:39 +0200

    .

--- MAI.py
+++ MAI.py
@@ -1,4 +1,4 @@
 from TWOD_RCI.RCI import rci
 
 while True:
-    print(rci(0))
+    print(rci(0), "2")
--- TWOD_RCI/RCI_ENC.py
+++ TWOD_RCI/RCI_ENC.py
@@ -1,7 +1,7 @@
 def rci_enc(rtx, drtx=-1, urtx=1, dsr=982, usr=2006):
 
     print((((abs(drtx) + urtx) / (usr - dsr)) * (int(rtx) - dsr)) + drtx)
-    print(int(rtx) + 5 / 20000)
+    print(int(rtx) / 1024 + 5)
 
 
 if __name__ == '__main__':
