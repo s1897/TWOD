18172ad6f86fd7b8cb31bedc4a25ab700b2f9826
s1897 <tillheim@bluewin.ch>
2020-04-12 12:08:26 +0200

    .

--- TWOD_RCI/RCI.py
+++ TWOD_RCI/RCI.py
@@ -6,7 +6,9 @@ check_apm()
 
 from RCI_ENC import rci_enc
 
-return(int(RCInput().read(1)))
+
+def rci(rc_channl)
+    return int(RCInput().read(rc_channl))
 
 # def rci(rc_channl, rc_drx=-1, rc_urx=1, rc_di=982, rc_ui=2006):
 #     return rci_enc(int(RCInput().read(rc_channl)), rc_drx, rc_urx, rc_di, rc_ui)
