6d7d49c8177d0223c33006e48966a98f6d16578e
s1897 <tillheim@bluewin.ch>
2020-04-12 14:28:23 +0200

    .

--- TWOD_RCI/RCI_ENC.py
+++ TWOD_RCI/RCI_ENC.py
@@ -1,2 +1,7 @@
-def rci_enc(rtx, drtx=1000, urtx=2000):
-    return rtx + drtx + urtx
+def rci_enc(rtx, drtx=-1, urtx=1, dsr=982, usr=2006):
+
+    print((((abs(drtx) + urtx) / (usr - dsr)) * (rtx - dsr)) + drtx)
+
+
+if __name__ == '__main__':
+    rci_enc(982)
