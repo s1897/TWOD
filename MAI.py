5bb59ce0e1746f3572196448df2a5806f5c4e85d
s1897 <tillheim@bluewin.ch>
2020-04-12 13:52:04 +0200

    .

--- MAI.py
+++ MAI.py
@@ -2,11 +2,5 @@ from TWOD_RCI.RCI import rci
 
 while True:
     receiver_imput = rci(0)
-    Convertet_output_lowes = -1
-    Convertet_output_highest = 1
-    receiver_imput_lowest = 982
-    receiver_imput_highest = 2006
 
-    receiver_signal_range = receiver_imput_highest - receiver_imput_lowest
-
-    print(((Convertet_output_highest - Convertet_output_lowes) / receiver_signal_range) * (receiver_signal_range - (receiver_imput_highest - receiver_imput))) + Convertet_output_lowes
+    print(receiver_imput)
