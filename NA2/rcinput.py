e65a2ccbe21fb27f82e8704975116a7da8b1a63c
s1897 <tillheim@bluewin.ch>
2020-04-14 01:42:31 +0200

    .

--- NA2/rcinput.py
+++ NA2/rcinput.py
@@ -7,11 +7,11 @@ class RCInput():
             try:
                 f = open("/sys/kernel/rcio/rcin/ch%d" % i, "r")
                 self.channels.append(f)
-            except: 
-                print ("Can't open file /sys/kernel/rcio/rcin/ch%d" % i)
-    
+            except:
+                #print ("Can't open file /sys/kernel/rcio/rcin/ch%d" % i)
+                continue
+
     def read(self, ch):
         value = self.channels[ch].read()
         position = self.channels[ch].seek(0, 0)
         return value[:-1]
-        
