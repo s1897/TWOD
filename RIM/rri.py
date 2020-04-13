ad9d88c913c709b16a8c254389dcddef550cd7dc
s1897 <tillheim@bluewin.ch>
2020-04-14 00:14:25 +0200

    .

--- RIM/rri.py
+++ RIM/rri.py
@@ -39,49 +39,51 @@ def rri(rpd):
     for tpn in rpd:
         tpd["tpn_" + tpn.split("_")[-1]] = None
 
+    # set psition control nummer
     pcn = 0
 
     # puts the register psition directory together
     while True:
 
+        # converted and add receiver input to temporary positions directory and update them
         for tpn in tpd:
             tpd[tpn] = rie(int(nri.read(int(tpn.split("_")[-1]))))
 
+        # tests whether one of the values ​​comes from the temporary positions directory above 0.9 and
+        # whether the register psition directory with the same number contains the value None.
         for tpn in tpd:
             if tpd[tpn] > 0.9 and rpd["rpn_" + tpn.split("_")[-1]] == None:
 
-                print(tpn, "1")
-
                 while True:
 
-                    # puts the convertet receiver imput into the receiver channel directory
+                    # update the convertet receiver imput into the receiver channel directory
                     tpd[tpn] = rie(int(nri.read(int(tpn.split("_")[-1]))))
 
+                    # tests whether one of the values ​​comes from the temporary positions directory under -0.9 and
+                    # whether the register psition directory with the same number contains the value None
                     if tpd[tpn] < -0.9 and rpd["rpn_" + tpn.split("_")[-1]] == None:
 
-                        print(tpn, "-1")
-
+                        # set the position value in the register psition directory
                         rpd["rpn_" + tpn.split("_")[-1]] = pcn
 
+                        # add one to psition control nummer and break the while loop
                         pcn += 1
-
                         break
 
+        # tests for the value to be deleted and whether it has already been deleted
         for rpn in rpd:
             if rpd[rpn] != None and "tpn_" + rpn.split("_")[-1] in tpd:
+
+                # deletes the previously tested value from the temporary positions directory.
                 tpd.__delitem__("tpn_" + rpn.split("_")[-1])
 
+        # the loop is interrupted when the temporary positions directory has no more value
         if tpd == dict():
             break
 
+    # returns register psition directory
     return rpd
 
 
 if __name__ == '__main__':
-    rcd = {'rcn_00': None, 'rcn_01': None, 'rcn_02': None, 'rcn_03': None, 'rcn_04': None, 'rcn_05': None, 'rcn_06': None, 'rcn_07': None}
-    rpd = {'rpn_00': None, 'rpn_01': None, 'rpn_02': None, 'rpn_03': None, 'rpn_04': None, 'rpn_05': None, 'rpn_06': None, 'rpn_07': None}
-    rri(rcd, rpd)
-
-    # puts the convertet receiver imputs into the receiver channel directory
-    # for rcn in rcd:
-    #     rcd[rcn] = rie(int(nri.read(int(rcn.split("_")[-1]))))
+    print("")
