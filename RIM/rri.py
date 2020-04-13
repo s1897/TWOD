c35bf6f7fbc3af581570b1ca8dfeec5b16d1146e
s1897 <tillheim@bluewin.ch>
2020-04-14 01:19:56 +0200

    .

--- RIM/rri.py
+++ RIM/rri.py
@@ -29,3 +29,13 @@ def rri(rcd, rpd):
         rcd["rcn_" + rpn.split("_")[-1]] = rie(int(nri.read(int(rpd[rpn]))))
 
     return rcd
+
+
+if __name__ == '__main__':
+
+    rcd = {'rcn_00': None, 'rcn_01': None, 'rcn_02': None, 'rcn_03': None, 'rcn_04': None, 'rcn_05': None, 'rcn_06': None, 'rcn_07': None}
+    rpd = {'rpn_00': 0, 'rpn_01': 1, 'rpn_02': 2, 'rpn_03': 3, 'rpn_04': 4, 'rpn_05': 5, 'rpn_06': 6, 'rpn_07': 7}
+
+    while True:
+        rcd = rri(rcd, rpd)
+        print(rcd)
