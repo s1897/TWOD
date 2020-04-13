80a21ba0abc74238f1160a038a48abccac5efab2
s1897 <tillheim@bluewin.ch>
2020-04-14 01:32:47 +0200

    .

--- EXA/RCInput.py
+++ EXA/RCInput.py
@@ -8,7 +8,15 @@ navio.util.check_apm()
 
 rcin = navio.rcinput.RCInput()
 
+period = []
+
 while (True):
-    period = rcin.read(2)
+    period.append(rcin.read(0))
+    period.append(rcin.read(1))
+    period.append(rcin.read(2))
+    period.append(rcin.read(3))
+    period.append(rcin.read(4))
+    period.append(rcin.read(5))
+    period.append(rcin.read(6))
+    period.append(rcin.read(7))
     print(period)
-    time.sleep(0.001)
