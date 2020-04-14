f33a648f4edd5e986ba88f5d4cf79caf5e95cdf9
s1897 <tillheim@bluewin.ch>
2020-04-15 01:11:07 +0200

    .

--- NA2/rcinput.py
+++ NA2/rcinput.py
@@ -6,27 +6,12 @@ class RCInput():
         for i in range(0, self.CHANNEL_COUNT):
             try:
                 f = open("/sys/kernel/rcio/rcin/ch%d" % i, "r")
-                self.channels.append(f.read()[:-1])
+                self.channels.append(f)
             except:
                 continue
                 # print("Can't open file /sys/kernel/rcio/rcin/ch%d" % i)
 
-            print(self.channels)
-
     def read(self, ch):
-        value = self.channels[ch]
-        # position = self.channels[ch].seek(0, 0)
+        value = self.channels[ch].read()
+        position = self.channels[ch].seek(0, 0)
         return value[:-1]
-
-
-if __name__ == '__main__':
-    h = RCInput()
-
-    while True:
-
-        t = []
-
-        for r in range(8):
-            t.append(h.read(r))
-
-        print(t)
--- TES/tes.py
+++ TES/tes.py
@@ -1,17 +1,24 @@
-rcd = {'rcn_00': None, 'rcn_01': None, 'rcn_02': None, 'rcn_03': None, 'rcn_04': None, 'rcn_05': None, 'rcn_06': None, 'rcn_07': None}
+# rcd = {'rcn_00': None, 'rcn_01': None, 'rcn_02': None, 'rcn_03': None, 'rcn_04': None, 'rcn_05': None, 'rcn_06': None, 'rcn_07': None}
+# #
+# tpd = {}
+# for t in rcd:
+#     tpd["tpn_" + t.split("_")[-1]] = int(t.split("_")[-1])
 #
-tpd = {}
-for t in rcd:
-    tpd["tpn_" + t.split("_")[-1]] = int(t.split("_")[-1])
+#
+# tpd.__delitem__("tpn_01")
+# tpd.__delitem__("tpn_02")
+# tpd.__delitem__("tpn_03")
+# tpd.__delitem__("tpn_04")
+# tpd.__delitem__("tpn_05")
+# tpd.__delitem__("tpn_06")
+# tpd.__delitem__("tpn_07")
+# print(tpd)
+# if "tpn_00" in tpd:
+#     print("l")
 
+i = 0
 
-tpd.__delitem__("tpn_01")
-tpd.__delitem__("tpn_02")
-tpd.__delitem__("tpn_03")
-tpd.__delitem__("tpn_04")
-tpd.__delitem__("tpn_05")
-tpd.__delitem__("tpn_06")
-tpd.__delitem__("tpn_07")
-print(tpd)
-if "tpn_00" in tpd:
-    print("l")
+while True:
+
+    f = open("/sys/kernel/rcio/rcin/ch%d" % i, "r")
+    print(f.read())
