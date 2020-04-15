e30853bab87ead6c2c50e7df39f4f5eb370cf4b2
s1897 <tillheim@bluewin.ch>
2020-04-15 14:17:26 +0200

    .

--- RIM/rir.py
+++ RIM/rir.py
@@ -20,6 +20,8 @@ def rir(rid):
     for rin in rid:
         rid[rin] = rie(int(open("/sys/kernel/rcio/rcin/ch{}".format(int(rin.split("_")[-1])), "r").read()[:-1]))
 
+    return rid
+
 
 if __name__ == '__main__':
     rid = {'rin_00': None, 'rin_01': None, 'rin_02': None, 'rin_03': None, 'rin_04': None, 'rin_05': None, 'rin_06': None, 'rin_07': None}
