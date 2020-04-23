31329e60feb6ea767fe9dc5535c170a6758e17d6
s1897 <tillheim@bluewin.ch>
2020-04-23 18:04:49 +0200

    .

--- IMU/qar.py
+++ IMU/qar.py
@@ -48,8 +48,12 @@ mpu.initialize()
 
 # read and write the data of the LSM9DS1 and MPU9250 sensor to a variable
 
-gld = lms.getMotion9()
-gmd = mpu.getMotion9()
+gld = lms.getMotion9()  # 00 = ax, 01 = ay, 02 = az, 10 = gx, 11 = gy, 12 = gz, 20 = mx, 21 = my, 22 = mz
+gmd = mpu.getMotion9()  # 00 = ax, 01 = ay, 02 = az, 10 = gx, 11 = gy, 12 = gz, 20 = mx, 21 = my, 22 = mz
 
-print(gld, gld[0][0], gld[0][1], gld[0][2])
-# print(("Acc:" + " {:+6f} {:+6f} {:+6f} " + "Gyr:" + " {:+6f} {:+6f} {:+6f} " + "Mag:" + " {:+6f} {:+6f} {:+6f}").format(m9a[0], m9a[1], m9a[2], m9g[0], m9g[1], m9g[2], m9m[0], m9m[1], m9m[2]))
+print()
+print(("lsm " + "Acc:" + " {:+6f} {:+6f} {:+6f} " + "Gyr:" + " {:+6f} {:+6f} {:+6f} " + "Mag:" + " {:+6f} {:+6f} {:+6f}").format(
+    gld[0][0], gld[0][1], gld[0][2], gld[1][0], gld[1][1], gld[1][2], gld[2][0], gld[2][1], gld[2][2]))
+
+print(("mpu " + "Acc:" + " {:+6f} {:+6f} {:+6f} " + "Gyr:" + " {:+6f} {:+6f} {:+6f} " + "Mag:" + " {:+6f} {:+6f} {:+6f}").format(
+    gmd[0][0], gmd[0][1], gmd[0][2], gmd[1][0], gmd[1][1], gmd[1][2], gmd[2][0], gmd[2][1], gmd[2][2]))
