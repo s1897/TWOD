d57b3abd12be4bbf89bc3529dcd63ebfc84a3606
s1897 <tillheim@bluewin.ch>
2020-04-24 01:42:09 +0200

    .

--- IMU/qar.py
+++ IMU/qar.py
@@ -42,53 +42,53 @@ def qar():
     nsv = 0
     sdd = rsd()
 
-    # for ml in sdd:
-    #     for agm in sdd[ml]:
-    #         for xyz in sdd[ml][agm]:
-    #             nsv += sdd[ml][agm][xyz] * sdd[ml][agm][xyz]
-    #
-    #     nsv **= 0.5
-    #
-    #     for xyz in sdd[ml][agm]:
-    #         sdd[ml][agm][xyz] /= nsv
+    for ml in sdd:
+        for agm in sdd[ml]:
+            for xyz in sdd[ml][agm]:
+                nsv += sdd[ml][agm][xyz] * sdd[ml][agm][xyz]
+
+        nsv **= 0.5
+
+        for xyz in sdd[ml][agm]:
+            sdd[ml][agm][xyz] /= nsv
 
     csn = list()
 
     # sdd now normalizd
     for ml in sdd:
-        if sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] > 0:
+        if sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["y"] > 0 and sdd[ml]["acc"]["z"] > 0:
             csn.append("fhk")   # fhk = +x+y+z cube
 
-        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] < 0:
+        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["y"] > 0 and sdd[ml]["acc"]["z"] < 0:
             csn.append("fhl")   # fhl = +x+y-z cube
 
-        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] > 0:
+        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["y"] < 0 and sdd[ml]["acc"]["z"] > 0:
             csn.append("fjk")   # fjk = +x-y+z cube
 
-        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] < 0:
+        elif sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["y"] < 0 and sdd[ml]["acc"]["z"] < 0:
             csn.append("fjl")   # fjl = +x-y-z cube
 
-        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] > 0:
+        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["y"] > 0 and sdd[ml]["acc"]["z"] > 0:
             csn.append("ghk")   # ghk = -x+y+z cube
 
-        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] > 0 and sdd[ml]["acc"]["x"] < 0:
+        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["y"] > 0 and sdd[ml]["acc"]["z"] < 0:
             csn.append("ghl")   # ghl = -x+y-z cube
 
-        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] > 0:
+        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["y"] < 0 and sdd[ml]["acc"]["z"] > 0:
             csn.append("gjk")   # gjk = -x-y+z cube
 
-        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["x"] < 0:
+        elif sdd[ml]["acc"]["x"] < 0 and sdd[ml]["acc"]["y"] < 0 and sdd[ml]["acc"]["z"] < 0:
             csn.append("gjl")   # gjl = -x-y-z cube
 
     print(csn)
 
-    print(
-        "mpu9250: acc: x: {:+4f} y: {:+4f} z: {:+4f} gyr: x: {:+4f} y: {:+4f} z: {:+4f} mag: x: {:+4f} y: {:+4f} z {:+4f}  \n".format(
-            sdd["mpu9250"]["acc"]["x"], sdd["mpu9250"]["acc"]["y"], sdd["mpu9250"]["acc"]["z"], sdd["mpu9250"]["gyr"]["x"], sdd["mpu9250"]["gyr"]["y"], sdd["mpu9250"]["gyr"]["z"], sdd["mpu9250"]["mag"]["x"], sdd["mpu9250"]["mag"]["y"], sdd["mpu9250"]["mag"]["z"]
-        ) +
-        "lsm9ds1: acc: x: {:+4f} y: {:+4f} z: {:+4f} gyr: x: {:+4f} y: {:+4f} z: {:+4f} mag: x: {:+4f} y: {:+4f} z {:+4f}  ".format(
-            sdd["lsm9ds1"]["acc"]["x"], sdd["lsm9ds1"]["acc"]["y"], sdd["lsm9ds1"]["acc"]["z"], sdd["lsm9ds1"]["gyr"]["x"], sdd["lsm9ds1"]["gyr"]["y"], sdd["lsm9ds1"]["gyr"]["z"], sdd["lsm9ds1"]["mag"]["x"], sdd["lsm9ds1"]["mag"]["y"], sdd["lsm9ds1"]["mag"]["z"]
-        ))
+    # print(
+    #     "mpu9250: acc: x: {:+4f} y: {:+4f} z: {:+4f} gyr: x: {:+4f} y: {:+4f} z: {:+4f} mag: x: {:+4f} y: {:+4f} z {:+4f}  \n".format(
+    #         sdd["mpu9250"]["acc"]["x"], sdd["mpu9250"]["acc"]["y"], sdd["mpu9250"]["acc"]["z"], sdd["mpu9250"]["gyr"]["x"], sdd["mpu9250"]["gyr"]["y"], sdd["mpu9250"]["gyr"]["z"], sdd["mpu9250"]["mag"]["x"], sdd["mpu9250"]["mag"]["y"], sdd["mpu9250"]["mag"]["z"]
+    #     ) +
+    #     "lsm9ds1: acc: x: {:+4f} y: {:+4f} z: {:+4f} gyr: x: {:+4f} y: {:+4f} z: {:+4f} mag: x: {:+4f} y: {:+4f} z {:+4f}  ".format(
+    #         sdd["lsm9ds1"]["acc"]["x"], sdd["lsm9ds1"]["acc"]["y"], sdd["lsm9ds1"]["acc"]["z"], sdd["lsm9ds1"]["gyr"]["x"], sdd["lsm9ds1"]["gyr"]["y"], sdd["lsm9ds1"]["gyr"]["z"], sdd["lsm9ds1"]["mag"]["x"], sdd["lsm9ds1"]["mag"]["y"], sdd["lsm9ds1"]["mag"]["z"]
+    #     ))
 
 
 if __name__ == '__main__':
