a4de225797f3e50c891aad55058738083fc44604
s1897 <tillheim@bluewin.ch>
2020-04-23 18:30:57 +0200

    .

--- IMU/qar.py
+++ IMU/qar.py
@@ -31,22 +31,36 @@
 # cso = calculate sensor offset
 # nsv = normalizd sensor vectord
 
-# lms = LSM9DS1
+# lsm = LSM9DS1
 # mpu = MPU9250
+
+# al = acc LSM9DS1
+# gl = gyr LSM9DS1
+# ml = mag LSM9DS1
+# am = acc MPU9250
+# gm = gyr MPU9250
+# mm = mag MPU9250
+
 
 # import of required directories
 from NA2.lsm9ds1 import LSM9DS1
 from NA2.mpu9250 import MPU9250
 
 # set LSM9DS1 and MPU9250 sensor to a variable
-lms = LSM9DS1()
+lsm = LSM9DS1()
 mpu = MPU9250()
 
 # initialize the LSM9DS1 and MPU9250 sensor
-lms.initialize()
+lsm.initialize()
 mpu.initialize()
 
-# read and write the data of the LSM9DS1 and MPU9250 sensor to a variable
+while True:
 
-gld = lms.getMotion9()  # 00 = ax, 01 = ay, 02 = az, 10 = gx, 11 = gy, 12 = gz, 20 = mx, 21 = my, 22 = mz
-gmd = mpu.getMotion9()  # 00 = ax, 01 = ay, 02 = az, 10 = gx, 11 = gy, 12 = gz, 20 = mx, 21 = my, 22 = mz
+    # read and write the data of the LSM9DS1 and MPU9250 sensor to a variable
+    al, gl, ml = lsm.getMotion9()
+    am, gm, mm = mpu.getMotion9()
+
+    print(("Acc:" + " {:+6f} {:+6f} {:+6f} " + "Gyr:" + " {:+6f} {:+6f} {:+6f} " + "Mag:" + " {:+6f} {:+6f} {:+6f}").format(
+        al[0], al[1], al[2], gl[0], gl[1], gl[2], ml[0], ml[1], ml[2]))
+    print(("Acc:" + " {:+6f} {:+6f} {:+6f} " + "Gyr:" + " {:+6f} {:+6f} {:+6f} " + "Mag:" + " {:+6f} {:+6f} {:+6f}").format(
+        am[0], am[1], am[2], gm[0], gm[1], gm[2], mm[0], mm[1], mm[2]))
