9705546af15d2c8de50b9b19b3142b5bb97c6e4d
s1897 <tillheim@bluewin.ch>
2020-04-26 16:55:48 +0200

    .

--- IMU/rpd.py
+++ IMU/rpd.py
@@ -88,7 +88,7 @@ def rpd():
     csn = {"mpu9250": None, "lsm9ds1": None}
     res = {"mpu9250": {"XY": None, "XZ": None, "YZ": None}, "lsm9ds1": {"XY": None, "XZ": None, "YZ": None}}
 
-    print(sdd)
+    # print(sdd)
     # calculate te normaliz value
     for ml in sdd:
         nsv = 0
